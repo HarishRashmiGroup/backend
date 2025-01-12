355e47d3c9dc9f3c2a019e2a15ace9fe31d83f92
HarishRashmiGroup <harish.kumar@rashmigroup.com>
2025-01-12 18:56:23 +0530

    serverless function for vercel

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,33 @@
-import 'dotenv/config';
+// import 'dotenv/config';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors();
+//   await app.listen(process.env.PORT || 3000);
+//   const address = app.getHttpServer().address();
+//   console.log(`Server is running on port ${address.port}`);
+// }
+// bootstrap();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Handler, Context, Callback } from 'aws-lambda';
+import serverlessExpress from '@vendia/serverless-express';
+
+let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
-  const address = app.getHttpServer().address();
-  console.log(`Server is running on port ${address.port}`);
+  await app.init(); // Initialize the app for serverless
+  const expressApp = app.getHttpAdapter().getInstance();
+  server = serverlessExpress({ app: expressApp });
 }
-bootstrap();
+
+export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
+  if (!server) {
+    await bootstrap();
+  }
+  return server(event, context, callback);
+};
